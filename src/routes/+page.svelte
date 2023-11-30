56667ab972d031dee549ac8579363d15d6d141c9
Mathis <mathisjung02@gmail.com>
2023-11-30 02:23:15 +0100

    Svelte 5, Prettier, Linter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,131 +1,138 @@
-<script>
-  import kana from '$lib/kana.js';
+<script lang="ts">
+	import kana from '$lib/kana';
 
-  const TIMEOUT_DURATION = 800;
-  let points = 0;
-  let answer = '';
-  let wrongAnswer = false;
-  const kanaKeys = Object.keys(kana);
-  let currentKanaKey = getRandomKanaKey();
-  $: backgroundColor = wrongAnswer ? '#ffdddd' : '#f5f5f5';
-  let hiragana = true;
-  $: currentKanaDisplay = hiragana ? kana[currentKanaKey].hiragana : kana[currentKanaKey].katakana;
+	const TIMEOUT_DURATION = 800;
+	let points: number = $state(0);
+	let answer: string = $state('');
+	let wrongAnswer: boolean = $state(false);
 
-  function getRandomKanaKey() {
-    const randomIndex = Math.floor(Math.random() * kanaKeys.length);
-    return kanaKeys[randomIndex];
-  }
+	const kanaKeys: string[] = Object.keys(kana);
+	function getRandomKanaKey(): string {
+		const randomIndex = Math.floor(Math.random() * kanaKeys.length);
+		return kanaKeys[randomIndex];
+	}
+	let currentKanaKey = $state(getRandomKanaKey());
+	let hiragana = $state(true);
+	let currentKanaDisplay: string = $derived(
+		hiragana ? kana[currentKanaKey].hiragana : kana[currentKanaKey].katakana
+	);
 
-  function resetAnswer() {
-    answer = '';
-    wrongAnswer = false;
-  }
-
-  function checkAnswer() {
-  if (answer === '') return;
-    if (answer.toLowerCase() === currentKanaKey) {
-      points++;
-      currentKanaKey = getRandomKanaKey();
-      resetAnswer();
-    } else {
-      points = 0;
-      wrongAnswer = true;
-      setTimeout(resetAnswer, TIMEOUT_DURATION);
-    }
-  }
+	function resetAnswer() {
+		answer = '';
+		wrongAnswer = false;
+	}
+  
+	function checkAnswer() {
+		if (answer === '') return;
+		if (answer.toLowerCase() === currentKanaKey) {
+			points++;
+			currentKanaKey = getRandomKanaKey();
+			resetAnswer();
+		} else {
+			points = 0;
+			wrongAnswer = true;
+			setTimeout(resetAnswer, TIMEOUT_DURATION);
+		}
+	}
 </script>
 
 <div class="points-display">Points: {points}</div>
-<div class="container" style="background-color: {backgroundColor}">
-  <label for="kana-display" class="kana-display">{currentKanaDisplay}</label>
-  <input type="text" id="answer-input" bind:value={answer} on:keyup={event => event.key === 'Enter' && checkAnswer()}>
-  <button on:click={checkAnswer}>Submit</button>
+<div class:wrong_answer={wrongAnswer} class="container">
+	<label for="kana-display" class="kana-display">{currentKanaDisplay}</label>
+	<input
+		type="text"
+		id="answer-input"
+		bind:value={answer}
+		on:keyup={(event) => event.key === 'Enter' && checkAnswer()}
+	/>
+	<button on:click={checkAnswer}>Submit</button>
 </div>
 
 <div class="kana-infos">
-  <button class:active={hiragana} on:click={() => hiragana = true}>Hiragana</button>
-  <button class:active={!hiragana} on:click={() => hiragana = false}>Katakana</button>
+	<button class:active={hiragana} on:click={() => (hiragana = true)}>Hiragana</button>
+	<button class:active={!hiragana} on:click={() => (hiragana = false)}>Katakana</button>
 </div>
+
 <style>
-  .container {
-    background-color: #f5f5f5;
-    padding: 2rem;
-    border-radius: 10px;
-    width: 300px;
-    height: 150px;
-    margin: auto;
-    margin-bottom: 1rem; /* Ajout d'un espace en dessous du cadre pour les points */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    transition: background-color 0.3s; /* Transition pour l'effet rouge */
-  }
-.container.wrong {
-    background-color: #ffdddd; /* Couleur de fond pour mauvaise réponse */
-  }
+	.container {
+		background-color: #f5f5f5;
+		padding: 2rem;
+		border-radius: 10px;
+		width: 300px;
+		height: 150px;
+		margin: auto;
+		margin-bottom: 1rem; /* Ajout d'un espace en dessous du cadre pour les points */
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		transition: background-color 0.3s; /* Transition pour l'effet rouge */
+	}
 
+	.wrong_answer {
+		background-color: #ff5d5d !important;
+	}
 
-  .kana-display {
-    font-size: 3em;
-    margin: 0; /* Enlève les marges par défaut */
-    text-align: center;
-  }
+	.kana-display {
+		font-size: 3em;
+		margin: 0; /* Enlève les marges par défaut */
+		text-align: center;
+	}
 
-  input {
-    font-size: 1em;
-    padding: 0.5em;
-    margin-top: 1em; /* Espacement entre le kana et l'input */
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    width: 80%; /* Largeur relative au conteneur */
-    box-sizing: border-box;
-  }
+	input {
+		font-size: 1em;
+		padding: 0.5em;
+		margin-top: 1em; /* Espacement entre le kana et l'input */
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		width: 80%; /* Largeur relative au conteneur */
+		box-sizing: border-box;
+	}
 
-  button {
-    background-color: #e0e0e0;
-    color: #333;
-    border: none;
-    padding: 0.5em 1em;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 1em;
-  }
+	button {
+		background-color: #e0e0e0;
+		color: #333;
+		border: none;
+		padding: 0.5em 1em;
+		border-radius: 5px;
+		cursor: pointer;
+		margin-top: 1em;
+	}
 
-  .points-display {
-    font-weight: bold;
-    text-align: center;
-    font-size: 1.5em;
-    color: #4CAF50;
-    margin-bottom: 1rem; /* Espacement entre les points et le cadre */
-  }
+	.points-display {
+		font-weight: bold;
+		text-align: center;
+		font-size: 1.5em;
+		color: #4caf50;
+		margin-bottom: 1rem; /* Espacement entre les points et le cadre */
+	}
 
-  .kana-infos {
-    background-color: #f5f5f5;
-    padding: 2rem;
-    border-radius: 10px;
-    width: 150px;
-    height: 75px;
-    margin: auto;
-    margin-bottom: 1rem; /* Ajout d'un espace en dessous du cadre pour les points */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  .kana-infos button {
-    border: none;
-    padding: 0.5em 1em;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 1em;
-  }
+	.kana-infos {
+		background-color: #f5f5f5;
+		padding: 2rem;
+		border-radius: 10px;
+		width: 150px;
+		height: 75px;
+		margin: auto;
+		margin-bottom: 1rem; /* Ajout d'un espace en dessous du cadre pour les points */
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.kana-infos button {
+		border: none;
+		padding: 0.5em 1em;
+		border-radius: 5px;
+		cursor: pointer;
+		margin-top: 1em;
+	}
 
-  .kana-infos button.active {
-    background-color: #ffffff;
-    color: #000000;
-    cursor: default;
-  }
+	.kana-infos button.active {
+		background-color: #ffffff;
+		color: #000000;
+		cursor: default;
+	}
 </style>

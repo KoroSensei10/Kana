0f1c33aee5137a56b829ee63f57263a4f9960a86
Mathis <mathisjung02@gmail.com>
2023-11-30 01:31:55 +0100

    Ajout support Katakana

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,51 @@
 <script>
   import kana from '$lib/kana.js';
 
+  const TIMEOUT_DURATION = 800;
   let points = 0;
   let answer = '';
   let wrongAnswer = false;
   const kanaKeys = Object.keys(kana);
   let currentKanaKey = getRandomKanaKey();
   $: backgroundColor = wrongAnswer ? '#ffdddd' : '#f5f5f5';
+  let hiragana = true;
+  $: currentKanaDisplay = hiragana ? kana[currentKanaKey].hiragana : kana[currentKanaKey].katakana;
 
   function getRandomKanaKey() {
-      const randomIndex = Math.floor(Math.random() * kanaKeys.length);
-      return kanaKeys[randomIndex];
+    const randomIndex = Math.floor(Math.random() * kanaKeys.length);
+    return kanaKeys[randomIndex];
+  }
+
+  function resetAnswer() {
+    answer = '';
+    wrongAnswer = false;
   }
 
   function checkAnswer() {
-    if (answer === '') return;
-      if (answer.toLowerCase() === currentKanaKey) {
-          points++;
-          currentKanaKey = getRandomKanaKey();
-          wrongAnswer = false;
-      } else {
-          points = 0;
-          wrongAnswer = true;
-          setTimeout(() => {
-              wrongAnswer = false;
-          }, 800);
-      }
-      answer = '';
+  if (answer === '') return;
+    if (answer.toLowerCase() === currentKanaKey) {
+      points++;
+      currentKanaKey = getRandomKanaKey();
+      resetAnswer();
+    } else {
+      points = 0;
+      wrongAnswer = true;
+      setTimeout(resetAnswer, TIMEOUT_DURATION);
+    }
   }
 </script>
 
 <div class="points-display">Points: {points}</div>
 <div class="container" style="background-color: {backgroundColor}">
-  <label for="kana-display" class="kana-display">{kana[currentKanaKey].hiragana}</label>
+  <label for="kana-display" class="kana-display">{currentKanaDisplay}</label>
   <input type="text" id="answer-input" bind:value={answer} on:keyup={event => event.key === 'Enter' && checkAnswer()}>
   <button on:click={checkAnswer}>Submit</button>
 </div>
 
+<div class="kana-infos">
+  <button class:active={hiragana} on:click={() => hiragana = true}>Hiragana</button>
+  <button class:active={!hiragana} on:click={() => hiragana = false}>Katakana</button>
+</div>
 <style>
   .container {
     background-color: #f5f5f5;
@@ -90,5 +99,33 @@
     font-size: 1.5em;
     color: #4CAF50;
     margin-bottom: 1rem; /* Espacement entre les points et le cadre */
+  }
+
+  .kana-infos {
+    background-color: #f5f5f5;
+    padding: 2rem;
+    border-radius: 10px;
+    width: 150px;
+    height: 75px;
+    margin: auto;
+    margin-bottom: 1rem; /* Ajout d'un espace en dessous du cadre pour les points */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .kana-infos button {
+    border: none;
+    padding: 0.5em 1em;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-top: 1em;
+  }
+
+  .kana-infos button.active {
+    background-color: #ffffff;
+    color: #000000;
+    cursor: default;
   }
 </style>
